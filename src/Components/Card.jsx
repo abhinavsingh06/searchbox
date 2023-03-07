5a83f3805f5a9b202219044fcafc80fc9e2a69aa
abhinav <singh.abhinav6996@gmail.com>
2023-03-08 01:35:27 +0530

    chore: add search result functionality

--- src/Components/Card.jsx
+++ src/Components/Card.jsx
@@ -1,12 +1,42 @@
 import React from 'react'
 import '../stylesheets/card.css';
 
-export default function Card({id, name, address}) {
+export default function Card({id, name, address, searchInput, filteredResult}) {
   return (
     <div className='dataCard'>
-      <h1>{id}</h1>
-      <h3>{name}</h3>
-      <p>{address}</p>
+      <div>
+        {id.includes(searchInput) ?
+          (<span>
+            <span style={{ color: 'red' }}>{id.slice(0, searchInput.length)}</span>
+            <span>{id.slice(searchInput.length)}</span>
+          </span>) : (
+            <span>
+              <span>{id}</span>
+            </span>
+          )}
+      </div>
+      <div>
+      {name.toLowerCase().includes(searchInput.toLowerCase()) ?
+        (<span>
+          <span style={{ color: 'red' }}>{name.slice(0, searchInput.length)}</span>
+          <span>{name.slice(searchInput.length)}</span>
+        </span>) : (
+          <span>
+            <span>{name}</span>
+          </span>
+          )}
+      </div>
+      <div>
+      {address.toLowerCase().includes(searchInput.toLowerCase()) ?
+        (<span>
+          <span style={{ color: 'red' }}>{address.slice(0, searchInput.length)}</span>
+          <span>{address.slice(searchInput.length)}</span>
+        </span>) : (
+          <span>
+            <span>{address}</span>
+          </span>
+          )}
+        </div>
     </div>
   )
 }
--- src/Components/SearchBar.jsx
+++ src/Components/SearchBar.jsx
@@ -2,22 +2,42 @@ import React, { useState } from 'react'
 import '../stylesheets/searchbar.css';
 import Card from './Card';
 
-export default function SearchBox({ placeholder, data }) {
+export default function SearchBar({ placeholder, data }) {
   const [searchInput, setSearchInput] = useState('');
+  const [filteredResult, setFilteredResult] = useState([]);
+
+  const handleSearch = (event) => {
+    const value = event.target.value;
+    searchItems(value);
+  }
+
   const searchItems = (searchValue) => {
     setSearchInput(searchValue);
-  }
+    if (searchInput !== '') {
+      const filteredData = data.filter((item) => {
+        return Object.values(item).join('').toLowerCase().includes(searchInput.toLowerCase());
+      });
+      setFilteredResult(filteredData);
+    }
+    else {
+      setFilteredResult([]);
+    }
+  };
 
   return (
     <div className='search'>
       <div className='searchInput'>
-        <input type="text" placeholder={placeholder} data={data} onChange={(e) => searchItems(e.target.value)} />
+        <input type="text" placeholder={placeholder} data={data} onChange={handleSearch} />
       </div>
-      <div className='searchResult'>
-          {data.map(({id, name, address}) => (
-            <Card key={id} id={id} name={name} address={address} />
-          ))}
-      </div>
-    </div>
+      {
+        filteredResult?.length > 0 ? (
+          <div className='searchResult'>
+            {filteredResult.map(({ id, name, address }) => (
+              <Card key={id} id={id} name={name} address={address} searchInput={searchInput} filteredResult={filteredResult} />
+            ))}
+          </div>
+        ) : <div className='searchResult'>No user found</div>
+      }
+    </div>  
   )
 }
