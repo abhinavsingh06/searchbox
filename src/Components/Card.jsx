ce354fd9dee332bb955f64f98a1bed9058bdb47a
abhinav <singh.abhinav6996@gmail.com>
2023-03-09 13:50:34 +0530

    fix: search result functionality

--- src/Components/Card.jsx
+++ src/Components/Card.jsx
@@ -1,42 +1,35 @@
 import React from 'react'
 import '../stylesheets/card.css';
 
-export default function Card({id, name, address, searchInput, filteredResult}) {
+export default function Card({ id, name, address, pincode, searchInput, filteredResult }) {
+
+  function getHighlightedText(text, highlight) {
+    const parts = text.split(new RegExp(`(${highlight})`, 'gi'));
+    return (
+      <span>
+        {parts.map((part, i) => 
+          <span key={i} style={part.toLowerCase() === highlight.toLowerCase() ? { color: 'blue' } : {} }>
+            { part }
+          </span>)
+        }
+      </span>
+    );
+  }
+
   return (
     <div className='dataCard'>
-      <div>
-        {id.includes(searchInput) ?
-          (<span>
-            <span style={{ color: 'red' }}>{id.slice(0, searchInput.length)}</span>
-            <span>{id.slice(searchInput.length)}</span>
-          </span>) : (
-            <span>
-              <span>{id}</span>
-            </span>
-          )}
-      </div>
-      <div>
-      {name.toLowerCase().includes(searchInput.toLowerCase()) ?
-        (<span>
-          <span style={{ color: 'red' }}>{name.slice(0, searchInput.length)}</span>
-          <span>{name.slice(searchInput.length)}</span>
-        </span>) : (
-          <span>
-            <span>{name}</span>
-          </span>
-          )}
-      </div>
-      <div>
-      {address.toLowerCase().includes(searchInput.toLowerCase()) ?
-        (<span>
-          <span style={{ color: 'red' }}>{address.slice(0, searchInput.length)}</span>
-          <span>{address.slice(searchInput.length)}</span>
-        </span>) : (
-          <span>
-            <span>{address}</span>
-          </span>
-          )}
-        </div>
+      <h1>
+        {getHighlightedText(id,searchInput)}
+      </h1>
+      <h3>
+        {getHighlightedText(name,searchInput)}
+      </h3>
+      {
+        searchInput ? <div className='searchNotification'>{ searchInput } found in items</div> : ""
+      }
+      <p>
+        {getHighlightedText(address,searchInput)}
+      </p>
     </div>
   )
 }
--- src/Components/SearchBar.jsx
+++ src/Components/SearchBar.jsx
@@ -1,26 +1,31 @@
-import React, { useState } from 'react'
+import React, { useState, useRef, useEffect } from 'react'
 import '../stylesheets/searchbar.css';
 import Card from './Card';
 
 export default function SearchBar({ placeholder, data }) {
   const [searchInput, setSearchInput] = useState('');
   const [filteredResult, setFilteredResult] = useState([]);
-
+  const [searching, setSearching] = useState(false);
+  
   const handleSearch = (event) => {
     const value = event.target.value;
-    searchItems(value);
+    if (value.length === 0) {
+      setFilteredResult([])
+      setSearching(false);
+    }
+    if (value?.length > 0) {
+      searchItems(value);
+      setSearching(true);
+    }
   }
 
   const searchItems = (searchValue) => {
     setSearchInput(searchValue);
-    if (searchInput !== '') {
+    if (searchInput.length !== 0) {
       const filteredData = data.filter((item) => {
-        return Object.values(item).join('').toLowerCase().includes(searchInput.toLowerCase());
+        return Object.values(item).join(' ').toLowerCase().includes(searchValue.toLowerCase());
       });
       setFilteredResult(filteredData);
-    }
-    else {
-      setFilteredResult([]);
     }
   };
 
@@ -32,11 +37,19 @@ export default function SearchBar({ placeholder, data }) {
       {
         filteredResult?.length > 0 ? (
           <div className='searchResult'>
-            {filteredResult.map(({ id, name, address }) => (
-              <Card key={id} id={id} name={name} address={address} searchInput={searchInput} filteredResult={filteredResult} />
+            {filteredResult.map(({ id, name, address, pincode }, i) => (
+              <Card key={id} id={id} name={name} address={address} pincode={pincode} searchInput={searchInput} filteredResult={filteredResult} />
             ))}
           </div>
-        ) : <div className='searchResult'>No user found</div>
+          ) : (
+          <>
+            {searching ? (
+              <div className='searchResult'>No user found</div>
+            ) : (
+                null
+            )}
+          </>  
+        )
       }
     </div>  
   )
